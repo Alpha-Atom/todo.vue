6cd1add1c8510d2d3562ec832769caa6425ff5ab
Matt Coles <mattc65@gmail.com>
2016-08-23 12:12:04 +0100

    Add some simple localStorage support so that todos persist with page reload

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-var Vue = require('vue');
+var Vue = require('vue');;
+
+var existingTodos = localStorage.getItem('todos')
 
 var app = new Vue({
   el: '#app',
   data: {
     title: "Todo List",
     newTodo: '',
-    todos: [],
+    todos: existingTodos ? JSON.parse(existingTodos) : []
   },
   methods: {
     add: function () {
       var text = this.newTodo.trim()
       if (text) {
         this.todos.push({ text: text, complete: false, style: {}})
+        this.save()
         this.newTodo = ''
       }
     },
     remove: function (idx) {
       this.todos.splice(idx, 1)
+      this.save()
     },
     complete: function (idx) {
       if (this.todos[idx].complete === false) {
@@ -26,18 +30,25 @@ var app = new Vue({
         this.$set('todos["'+ idx + '"]["style"]["text-decoration"]', 'none')
         this.$set('todos["' + idx + '"]["complete"]', false)
       }
+      this.save()
     },
     up: function (idx) {
       var elAbove = JSON.parse(JSON.stringify(this.todos[idx-1]))
       var elBelow = JSON.parse(JSON.stringify(this.todos[idx]))
       this.$set('todos["' + (idx-1) + '"]', elBelow)
       this.$set('todos["' + idx + '"]', elAbove)
+      this.save()
     },
     down: function (idx) {
       var elAbove = JSON.parse(JSON.stringify(this.todos[idx]))
       var elBelow = JSON.parse(JSON.stringify(this.todos[idx+1]))
       this.$set('todos["' + (idx+1) + '"]', elAbove)
       this.$set('todos["' + idx + '"]', elBelow)
+      this.save()
+    },
+    save: function () {
+      var localTodos = JSON.stringify(this.todos)
+      localStorage.setItem('todos', localTodos)
     }
   }
 })
